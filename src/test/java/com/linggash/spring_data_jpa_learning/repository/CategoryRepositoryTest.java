c91e1613a176b85ade588ea39d20bd3badd465e5
linggash <sempanalingga@gmail.com>
2024-09-25 14:30:18 +0700

    Learned about Crud Repository

---
+++ src/test/java/com/linggash/spring_data_jpa_learning/repository/CategoryRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.linggash.spring_data_jpa_learning.repository;
+
+import com.linggash.spring_data_jpa_learning.entity.Category;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void insert() {
+        Category category = new Category();
+        category.setName("GADGET");
+
+        categoryRepository.save(category);
+
+        assertNotNull(category.getId());
+    }
+
+    @Test
+    void update() {
+        Category category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+
+        category.setName("GADGET MURAH");
+        categoryRepository.save(category);
+
+        category = categoryRepository.findById(1L).orElse(null);
+        assertNotNull(category);
+        assertEquals("GADGET MURAH", category.getName());
+    }
+}
