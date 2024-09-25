a6abcc123106159c60ba42ee367f1dbfd5ed6c54
linggash <sempanalingga@gmail.com>
2024-09-25 10:18:48 +0700

    Init project

---
+++ src/main/java/com/linggash/spring_data_jpa_learning/SpringDataJpaLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_data_jpa_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linggash/spring_data_jpa_learning/SpringDataJpaLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_data_jpa_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
